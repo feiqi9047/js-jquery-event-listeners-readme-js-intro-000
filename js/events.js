99d663ba18b541def93d04283cd2f062816a3fca
Fei Qi <feiqi9047@gmail.com>
2018-11-20 15:35:06 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,9 @@ function frameIt(){
 
 function pressIt(){
   $(document).on('keydown', function(key){
-    if(key.which ==)
+    if(key.which ==71){
+      alert('g was pressed!')
+    }
   })
 }
 
